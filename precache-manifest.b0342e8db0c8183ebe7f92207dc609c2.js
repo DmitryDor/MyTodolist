ad59e11720ac917634fac4b750fb1cf9c74e7b8f
Your NameDmitryDor <you@example.com>
2021-03-31 12:38:59 +0300

    Updates

---
+++ precache-manifest.b0342e8db0c8183ebe7f92207dc609c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fda37fd2e4fcce2eb191d4afbbae7fdc",
+    "url": "/MyTodolist/index.html"
+  },
+  {
+    "revision": "26056a2d11d42fe582ec",
+    "url": "/MyTodolist/static/css/main.ab0ef28a.chunk.css"
+  },
+  {
+    "revision": "a5fb2c0d02634ea20de9",
+    "url": "/MyTodolist/static/js/2.e0f53a87.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/MyTodolist/static/js/2.e0f53a87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26056a2d11d42fe582ec",
+    "url": "/MyTodolist/static/js/main.ff602f67.chunk.js"
+  },
+  {
+    "revision": "d9f7504b3a2b21e8fa72",
+    "url": "/MyTodolist/static/js/runtime-main.51f8ee1b.js"
+  }
+]);
